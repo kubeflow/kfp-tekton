ab4a6f123832c1a422c1bcf65d4d9709e0bcffe9
Christian Kadner <ckadner@us.ibm.com>
2020-06-12 16:13:40 -0700

    Generate long_description for PyPi from sdk/README.md
    - Add installation from PyPi to sdk/README.md
    - Modify setup.py to read long_description from sdk/README.md
    - Replace relative links with absolute links to kfp-tekton GitHub repo
    - Add make distribution target for upload to testpypi
    
    Resolves #185

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -12,32 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import re
 
+# =============================================================================
+# To install the kfp_tekton project run:
+#
+#    $ pip3 install -e .
+#
+# To create a distribution for PyPi run:
+#
+#    $ export KFP_TEKTON_VERSION=0.1.2
+#    $ python3 setup.py sdist
+#    $ twine check dist/kfp-tekton-${KFP_TEKTON_VERSION}.tar.gz
+#    $ twine upload --repository pypi dist/kfp-tekton-${KFP_TEKTON_VERSION}.tar.gz
+#
+#   ... or:
+#
+#    $ make distribution KFP_TEKTON_VERSION=0.1.2
+#
+# =============================================================================
+
+import logging
+import re
+import sys
+
+from os import environ as env
+from os.path import abspath, dirname, join
 from setuptools import setup
 
 
 NAME = "kfp-tekton"
-# VERSION = .... Change the version in kfp_tekton/__init__.py
+# VERSION = ... set the version in kfp_tekton/__init__.py or use KFP_TEKTON_VERSION env var
 LICENSE = "Apache 2.0"
 HOMEPAGE = "https://github.com/kubeflow/kfp-tekton/"
-
 DESCRIPTION = "Tekton Compiler for Kubeflow Pipelines"
 LONG_DESCRIPTION = """\
 The kfp-tekton project is an extension of the Kubeflow Pipelines SDK compiler
 generating Tekton YAML instead of Argo YAML. The project is still in an early
-development stage. Contributors are welcome: {}
+development stage. Contributions are welcome: {}
 """.format(HOMEPAGE)
 
 REQUIRES = [
     'kfp==0.5.1',
 ]
 
+logging.basicConfig()
+logger = logging.getLogger("kfp_tekton/setup.py")
+
 
 def find_version(*file_path_parts):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, *file_path_parts), 'r') as fp:
+
+    if "KFP_TEKTON_VERSION" in env:
+        version = env.get("KFP_TEKTON_VERSION")
+        logger.warning("Using KFP_TEKTON_VERSION={}".format(version))
+        return version
+
+    here = abspath(dirname(__file__))
+    with open(join(here, *file_path_parts), 'r') as fp:
         version_file_text = fp.read()
 
     version_match = re.search(
@@ -51,11 +81,86 @@ def find_version(*file_path_parts):
     raise RuntimeError("Unable to find version string.")
 
 
+def get_long_description() -> str:
+    """Extract the content of the sdk/README.md and replace relative links when
+    running `python3 setup.py sdist`. Return the abbreviated LONG_DESCRIPTION when
+    running `pip install .`
+
+    :return: long_description, Github flavored Markdown from the sdk/README.md
+    """
+    if "sdist" not in sys.argv:
+        # log messages are only displayed when running `pip --verbose`
+        logger.warning("This not a distribution build. Using abbreviated "
+                       "long_description: {}".format(LONG_DESCRIPTION))
+        return LONG_DESCRIPTION
+    else:
+        logger.warning(
+            "Building a distribution. Using the sdk/README.md for the "
+            "long_description to be displayed on the PyPi landing page.")
+
+    here = abspath(dirname(__file__))
+    project_root = join(here, "..", "..")
+    sdk_readme_file = join(project_root, "sdk", "README.md")
+
+    with open(sdk_readme_file, "r") as f:
+        long_description = f.read()
+
+    # replace relative links from sdk/README.md with be absolute links to GitHub
+    github_repo_master_path = "{}/blob/master".format(HOMEPAGE.rstrip("/"))
+
+    # replace relative links that are siblings to the README, i.e. [link text](FEATURES.md)
+    long_description = re.sub(
+        r"\[([^]]+)\]\((?!http|#|/)([^)]+)\)",
+        r"[\1]({}/{}/\2)".format(github_repo_master_path, "sdk"),
+        long_description)
+
+    # replace links that are relative to the project root, i.e. [link text](/sdk/FEATURES.md)
+    long_description = re.sub(
+        r"\[([^]]+)\]\(/([^)]+)\)",
+        r"[\1]({}/\2)".format(github_repo_master_path),
+        long_description)
+
+    # add anchor link targets around headings for links from table-of-contents (TOC)
+    text = []
+    for line in long_description.splitlines():
+        matches = re.match(r'^(#+) (.*)$', line)
+        if matches:
+            line = '<h{}><a id="{}">{}</a></h{}>'.format(
+                len(matches.group(1)),
+                re.sub(r"[`.()]", "", matches.group(2).replace(" ", "-").lower()),
+                matches.group(2),
+                len(matches.group(1))
+            )
+        text.append(line)
+    long_description = "\n".join(text)
+
+    # verify all replaced links
+    import requests  # top-level import fails pip install, only required for make dist
+    for (text, link) in re.findall(r"\[([^]]+)\]\((%s[^)]+)\)" % github_repo_master_path,
+                                   long_description):
+        response = requests.head(link, allow_redirects=True, timeout=3)
+        if response.status_code >= 400:
+            # raise RuntimeError(  # don't fail the installation
+            # report errors when pip install verbose -v
+            logger.error(
+                "Invalid link in long_description: `[{}]({})`. "
+                "Please open an issue at {}/issues".format(
+                    text, link, HOMEPAGE.rstrip("/")))
+
+    # generate the README with absolute links for easy side-by-side verification
+    sdk_readme_w_abs_links = join(project_root, "sdk", "README_for_PyPi.md")
+    with open(sdk_readme_w_abs_links, 'w') as f:
+        f.write(long_description)
+
+    return long_description
+
+
 setup(
     name=NAME,
     version=find_version("kfp_tekton", "__init__.py"),
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     author="kubeflow.org",
     license=LICENSE,
     url=HOMEPAGE,
