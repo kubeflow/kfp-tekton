5515d9c1c92cb03f4864586d3d8d8c33c8bdcd17
Christian Kadner <ckadner@us.ibm.com>
2020-06-30 17:32:11 -0700

    Prepare release 0.2.0
    * Update version from 0.1.0 to 0.2.0

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -20,14 +20,14 @@
 #
 # To create a distribution for PyPi run:
 #
-#    $ export KFP_TEKTON_VERSION=0.1.2
+#    $ export KFP_TEKTON_VERSION=0.2.0-rc1
 #    $ python3 setup.py sdist
 #    $ twine check dist/kfp-tekton-${KFP_TEKTON_VERSION}.tar.gz
 #    $ twine upload --repository pypi dist/kfp-tekton-${KFP_TEKTON_VERSION}.tar.gz
 #
 #   ... or:
 #
-#    $ make distribution KFP_TEKTON_VERSION=0.1.2
+#    $ make distribution KFP_TEKTON_VERSION=0.2.0-rc1
 #
 # =============================================================================
 
