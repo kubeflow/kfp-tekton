60a8c4485a9a3ca1b7a9d162cfb34de1ced15029
Christian Kadner <ckadner@us.ibm.com>
2020-07-10 16:59:22 -0700

    Include sdk/FEATURES.md in Table-of-Contents check (#223)
    * Add TOC marker to sdk/FEATURES.md
    * Update tools/mdtoc.sh:
      * Include 1st level headings
      * Exclude triple-tick code blocks when looking for
        headings "# ..."
    * Update make check_mdtoc:
      * Tolerate + and * as TOC bullet points
      * Tolerate additional links appended to TOC entries,
        i.e. " - [Text](#achor-link) [Tracking issue][...]"

--- tools/mdtoc.sh
+++ tools/mdtoc.sh
@@ -16,12 +16,14 @@
 
 # This script will generate a table of contents (ToC) for Markdown (MD) files.
 #
-#  1. Find the paragraph headings with grep (2nd and 3rd level heading starting with "##" and "###")
-#  2. Extract the heading's text with sed and transform into '|' separated records of the form '###|Full Text|Full Text'
-#  3. Generate the ToC lines with awk by replacing '#' with '  ', converting spaces to dashes '-',
-#     removing special chars (like back-ticks, dot, parenthesis, colon) from the hyperlink,
-#     and lower-casing all capital letters
-#  4. Replace leading 2 spaces since our ToC does not include 1st level headings
+#  1. Remove the paragraphs (headings) above the "Table of Contents"
+#  2. Remove code blocks fenced by tripple back-ticks, to not treat #-comments as markdown headings
+#  3. Find the paragraph headings with grep (1st through 4th level heading starting with "#" and "####")
+#  4. Extract the heading's text with sed and transform into '|'-separated records of the form '###|Full Text|Full Text'
+#  5. Generate the ToC lines with awk by replacing '#' with '  ', converting spaces to dashes '-',
+#     removing special chars (like back-ticks, dot, parenthesis, colon, comma) from TOC anchor links,
+#     and lower-case all capital letters
+#  6. Remove leading 2 spaces in case ToC does not include 1st level headings, otherwise the TOC becomes a code block
 #
 # Inspired by https://medium.com/@acrodriguez/one-liner-to-generate-a-markdown-toc-f5292112fd14
 
@@ -29,7 +31,9 @@ SEP="|"
 
 [ -z "${1}" ] && echo -e "Usage:\n\n   $BASH_SOURCE <markdown file>\n" && exit 1
 
-grep -E "^#{2,3}" "${1}" | grep -v "Table of Contents" | \
+sed -n '/Table of Contents/,$p' "${1}" | tail -n +2 | \
+sed '/^```/,/^```/d' | \
+grep -E "^#{1,4}" | \
 sed -E "s/(#+) (.+)/\1${SEP}\2${SEP}\2/g" | \
-awk -F "${SEP}" '{ gsub(/#/,"  ",$1); gsub(/[ ]/,"-",$3); gsub(/[`.():]/,"",$3); print $1 "- [" $2 "](#" tolower($3) ")" }' | \
+awk -F "${SEP}" '{ gsub(/#/,"  ",$1); gsub(/[ ]/,"-",$3); gsub(/[`.():,]/,"",$3); print $1 "- [" $2 "](#" tolower($3) ")" }' | \
 sed -e 's/^  //g'
