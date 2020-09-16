138f91fd478823ad91e2f7190651acd6d81eb942
Tommy Li <Tommy.chaoping.li@ibm.com>
2020-09-16 12:00:11 -0700

    Add python 3.5 error comments

--- sdk/python/tests/compiler/compiler_tests.py
+++ sdk/python/tests/compiler/compiler_tests.py
@@ -184,6 +184,8 @@ class TestTektonCompiler(unittest.TestCase):
     """
     Test compiling a workflow with non configurable output file.
     """
+    # OrderedDict sorting before Python 3.6 within _verify_compiled_workflow 
+    # will fail on certain special characters.
     if sys.version_info < (3, 6, 0):
       logging.warning("Skipping hidden_output workflow test for Python version < 3.6.0")
     else:
