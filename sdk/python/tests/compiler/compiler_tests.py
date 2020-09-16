ec3b3b44239fc3b1888b4072455b1bcc8684b039
Tommy Li <Tommy.chaoping.li@ibm.com>
2020-09-16 12:09:48 -0700

    lint tailing whitespace

--- sdk/python/tests/compiler/compiler_tests.py
+++ sdk/python/tests/compiler/compiler_tests.py
@@ -184,7 +184,7 @@ class TestTektonCompiler(unittest.TestCase):
     """
     Test compiling a workflow with non configurable output file.
     """
-    # OrderedDict sorting before Python 3.6 within _verify_compiled_workflow 
+    # OrderedDict sorting before Python 3.6 within _verify_compiled_workflow
     # will fail on certain special characters.
     if sys.version_info < (3, 6, 0):
       logging.warning("Skipping hidden_output workflow test for Python version < 3.6.0")
