7dee25f60e98e70f1c9b4b5c3be8143b6102d1a4
Jin Chi He <hejinchi@cn.ibm.com>
2021-01-11 17:10:56 +0800

    Enhance Any Sequencer

--- tekton-catalog/any-sequencer/cmd/main.go
+++ tekton-catalog/any-sequencer/cmd/main.go
@@ -30,10 +30,20 @@ import (
 	"knative.dev/pkg/apis"
 )
 
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == strings.TrimSpace(str) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	var namespace string
 	var prName string
 	var taskList string
+	var failedTasks []string
 
 	flag.StringVar(&namespace, "namespace", "", "The namespace of the pipelinerun.")
 	flag.StringVar(&prName, "prName", "", "The name of the pipelinerun.")
@@ -64,22 +74,33 @@ func main() {
 	}
 
 	labelSelector := "tekton.dev/pipelineRun=" + strings.TrimSpace(prName)
-	watcher, err := tektonClient.TektonV1beta1().TaskRuns(namespace).Watch(context.TODO(), metav1.ListOptions{LabelSelector: labelSelector})
 
-	if err != nil {
-		log.Printf("TaskRun Watcher error:" + err.Error())
-		log.Printf("Please ensure the service account has permission to get taskRun.")
-		os.Exit(1)
-	}
+	for {
+		watcher, err := tektonClient.TektonV1beta1().TaskRuns(namespace).Watch(context.TODO(), metav1.ListOptions{LabelSelector: labelSelector})
 
-	for event := range watcher.ResultChan() {
-		taskrun := event.Object.(*v1beta1.TaskRun)
-		taskLabel := taskrun.Labels["tekton.dev/pipelineTask"]
-		for _, item := range tasks {
-			if strings.TrimSpace(item) == taskLabel {
+		if err != nil {
+			log.Printf("TaskRun Watcher error:" + err.Error())
+			log.Printf("Please ensure the service account has permission to get taskRun.")
+			os.Exit(1)
+		}
+
+		for event := range watcher.ResultChan() {
+			taskrun := event.Object.(*v1beta1.TaskRun)
+			taskLabel := taskrun.Labels["tekton.dev/pipelineTask"]
+			if contains(tasks, taskLabel) {
 				if taskrun.Status.GetCondition(apis.ConditionSucceeded).IsTrue() {
 					log.Printf("The TaskRun of %s succeeded.", taskLabel)
 					watcher.Stop()
+					os.Exit(0)
+				} else if taskrun.Status.GetCondition(apis.ConditionSucceeded).IsFalse() {
+					if !contains(failedTasks, taskLabel) {
+						failedTasks = append(failedTasks, taskLabel)
+					}
+					if len(failedTasks) >= len(tasks) {
+						log.Printf("All specified TaskRun(s) failed.")
+						watcher.Stop()
+						os.Exit(1)
+					}
 				}
 			}
 		}
