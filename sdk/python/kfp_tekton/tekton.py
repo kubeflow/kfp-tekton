836d2ee4264f513a023ea5e2bc715ac25b0e3671
Tommy Li <Tommy.chaoping.li@ibm.com>
2021-03-17 15:47:52 -0700

    rephrase wording

--- sdk/python/kfp_tekton/tekton.py
+++ sdk/python/kfp_tekton/tekton.py
@@ -20,7 +20,8 @@ from kfp_tekton.compiler._k8s_helper import sanitize_k8s_name
 
 
 class AnySequencer(ContainerOp):
-    """A containerOp that will proceed when any dependent condition met
+    """A containerOp that will proceed when any of the dependent containerOps completed
+       successfully
 
     Args:
         name: The name of the containerOp. It does not have to be unique within a pipeline
