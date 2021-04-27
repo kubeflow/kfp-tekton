8590671552d64aec47a1b2070d6539f9c002fd5b
Christian Kadner <ckadner@us.ibm.com>
2021-04-27 13:09:00 -0700

    Regenerate E2E logs after updating "golden" YAML files of unit tests

--- sdk/python/tests/compiler/testdata/withitem_multi_nested.py
+++ sdk/python/tests/compiler/testdata/withitem_multi_nested.py
@@ -25,7 +25,7 @@ class Coder:
 TektonCompiler._get_unique_id_code = Coder.empty
 
 
-@dsl.pipeline(name='my-pipeline')
+@dsl.pipeline(name='withitem multiple nesting pipeline')
 def pipeline(my_pipe_param: list = [100, 200], my_pipe_param3: list = [1, 2]):
     loop_args = [{'a': 1, 'b': 2}, {'a': 10, 'b': 20}]
     with dsl.ParallelFor(loop_args) as item:
