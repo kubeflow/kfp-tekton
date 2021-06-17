8fd39b670411d0faa2777d1a640e66b328865243
tomcli <tommy.chaoping.li@ibm.com>
2021-06-17 14:42:19 -0700

    fix alpine commands and add licenses

--- sdk/python/tests/compiler/testdata/nested_recur_params.py
+++ sdk/python/tests/compiler/testdata/nested_recur_params.py
@@ -1,7 +1,29 @@
+# Copyright 2021 kubeflow.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from kfp import dsl
 from kfp.components import load_component_from_text
 from kfp_tekton.tekton import CEL_ConditionOp
+from kfp_tekton.compiler import TektonCompiler
 
+
+class Coder:
+    def empty(self):
+        return ""
+
+
+TektonCompiler._get_unique_id_code = Coder.empty
 
 PrintOp = load_component_from_text("""
   name: print
@@ -13,8 +35,7 @@ PrintOp = load_component_from_text("""
     container:
       image: alpine:3.6
       command:
-      - concat:
-        - "echo "
+        - "echo"
         - { inputValue: msg }
 """)
 
