933bd02e457fb9ee9c10cc693d585deb7ca070cf
Christian Kadner <ckadner@us.ibm.com>
2021-11-18 14:38:38 -0800

    Run performance tests in parallel
    Pipeline compilation still runs in sequence due to
    compiler implementation limitations.
    
    Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

--- sdk/python/tests/compiler/performance_tests.py
+++ sdk/python/tests/compiler/performance_tests.py
@@ -14,11 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import concurrent.futures
 import datetime
+import functools
 import os
-# import sys
+import sys  # noqa
 import tempfile
 import time
+import threading
 
 from datetime import datetime as dt
 from os import environ as env
@@ -34,17 +37,26 @@ from kfp_tekton.compiler.pipeline_utils import TektonPipelineConf
 #  load test settings from environment variables
 # =============================================================================
 
-# TODO: remove defaults below, add instructions and/or script parameters
+# TODO: turn env vars into script parameters
 PUBLIC_IP = env.get("PUBLIC_IP")
 NAMESPACE = env.get("NAMESPACE", None)
 USER_INFO = env.get("USER_INFO")
 CONNECT_SID = env.get("CONNECT_SID")
+NUM_WORKERS = int(env.get("NUM_WORKERS", 1))
+EXPERIMENT = env.get("EXPERIMENT_NAME", "PERF_TEST")
+OUTPUT_FILE = env.get("OUTPUT_FILE", f"perf_test_{dt.now().strftime('%Y%m%d_%H%M%S')}_{PUBLIC_IP}.csv")
+OUTPUT_SEP = env.get("OUTPUT_SEP", ",")
 
-print(f"Environment variables:\n"
+
+print(f"Environment variables:\n\n"
       f"  PUBLIC_IP:   {PUBLIC_IP}\n"
       f"  NAMESPACE:   {NAMESPACE}\n"
       f"  USER_INFO:   {USER_INFO}\n"
-      f"  CONNECT_SID: {CONNECT_SID}\n")
+      f"  CONNECT_SID: {CONNECT_SID}\n"
+      f"  NUM_WORKERS: {NUM_WORKERS}\n"
+      f"  EXPERIMENT:  {EXPERIMENT}\n"
+      f"  OUTPUT_FILE: {OUTPUT_FILE}\n"
+      f"  OUTPUT_SEP:  {OUTPUT_SEP}\n")
 
 
 # =============================================================================
@@ -52,8 +64,6 @@ print(f"Environment variables:\n"
 # =============================================================================
 
 # kfp_tekton_root_dir = os.path.abspath(__file__).replace("sdk/python/tests/compiler/performance_tests.py", "")
-
-experiment_name = "PERF_TEST"
 
 
 def get_client() -> TektonClient:
@@ -64,10 +74,38 @@ def get_client() -> TektonClient:
     return client
 
 
+# method annotation to ensure the wrapped function is executed synchronously
+def synchronized(function):
+    lock = threading.Lock()
+
+    @functools.wraps(function)
+    def _synchronized_function(*args, **kwargs):
+        with lock:
+            result = function(*args, **kwargs)
+            return result
+
+    return _synchronized_function
+
+
+# TODO: synchronizing pipeline compilation skews recorded compile times since some
+#   pipelines will wait for others to be compiled, yet the recorded compilation
+#   start times are equal for all pipelines
+#   We need to change test design to run and time pipeline compilation sequentially
+#   and only execute pipelines in parallel
+if NUM_WORKERS > 1:
+    print("WARNING: pipeline compilation times are not accurate when running in parallel.\n")
+
+
+# TODO: cannot compile multiple pipelines in parallel due to use of static variables
+#   causing Exception "Nested pipelines are not allowed." in kfp/dsl/_pipeline.py
+#   def __enter__(self):
+#     if Pipeline._default_pipeline:
+#       raise Exception('Nested pipelines are not allowed.')
+@synchronized
 def compile_pipeline(pipeline_func: Callable) -> str:
     pipeline_name = pipeline_func.__name__
     file_name = pipeline_name + '.yaml'
-    tmpdir = tempfile.gettempdir()
+    tmpdir = tempfile.gettempdir()  # TODO: keep compiled pipelines?
     pipeline_package_path = os.path.join(tmpdir, file_name)
     pipeline_conf = TektonPipelineConf()
     TektonCompiler().compile(pipeline_func=pipeline_func,
@@ -81,9 +119,9 @@ def run_pipeline(pipeline_file: str,
                  arguments: Mapping[str, str] = None):
 
     client = get_client()
-    experiment = client.create_experiment(experiment_name)  # get or create
+    experiment = client.create_experiment(EXPERIMENT)  # get or create
     run_result = None
-    while run_result is None:
+    while run_result is None:  # TODO: add timeout or max retries on ApiException
         try:
             run_result: ApiRun = client.run_pipeline(
                 experiment_id=experiment.id,
@@ -91,7 +129,8 @@ def run_pipeline(pipeline_file: str,
                 pipeline_package_path=pipeline_file,
                 params=arguments)
         except ApiException as e:
-            print(f"KFP Server Exception: '{e.reason}' {e.status} '{e.body}' {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
+            print(f"KFP Server Exception: '{e.reason}' {e.status} '{e.body}'"
+                  f" {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
             time.sleep(1)
             client = get_client()
 
@@ -101,12 +140,13 @@ def run_pipeline(pipeline_file: str,
 def wait_for_run_to_complete(run_id: str) -> ApiRun:
     client = get_client()
     status = None
-    while status not in ["Succeeded", "Failed", "Error", "Skipped", "Terminated", "Completed", "CouldntGetTask"]:
+    while status not in ["Succeeded", "Failed", "Error", "Skipped", "Terminated",
+                         "Completed", "CouldntGetTask"]:
         try:
             run_detail: ApiRunDetail = client.get_run(run_id)
             run: ApiRun = run_detail.run
             status = run.status
-        except ApiException as e:
+        except ApiException as e:  # TODO: add timeout or max retries on ApiError
             print(f"KFP Server Exception: {e.reason}")
             time.sleep(1)
         time.sleep(0.1)
@@ -137,11 +177,11 @@ def load_pipeline_functions() -> [(Callable, str)]:
     from testdata.conditions_and_loops import conditions_and_loops
     pipeline_functions.append((conditions_and_loops, "conditions_and_loops"))
 
-    from testdata.condition_custom_task import flipcoin_pipeline
-    pipeline_functions.append((flipcoin_pipeline, "condition_custom_task"))
-
-    from testdata.loop_in_recursion import flipcoin as loop_in_loop
-    pipeline_functions.append((loop_in_loop, "loop_in_recursion"))
+    # from testdata.loop_in_recursion import flipcoin as loop_in_loop
+    # pipeline_functions.append((loop_in_loop, "loop_in_recursion"))
+    #
+    # from testdata.condition_custom_task import flipcoin_pipeline
+    # pipeline_functions.append((flipcoin_pipeline, "condition_custom_task"))
 
     # TODO: add more pipelines
 
@@ -153,39 +193,68 @@ def load_pipeline_functions() -> [(Callable, str)]:
     return pipeline_functions
 
 
-def run_performance_test():
+def run_single_pipeline_performance_test(pipeline_func: Callable, run_name: str) -> ApiRun:
+    t = [dt.now()]
 
-    output_file = f"performance_test_times_{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-    output_sep = ","
+    pipeline_file = compile_pipeline(pipeline_func)
+    t += [dt.now()]
 
-    with open(output_file, "w") as f:
-        f.write(output_sep.join(["Pipeline", "Compile", "Submit", "Run"]) + "\n")
+    run_id = run_pipeline(pipeline_file, run_name)
+    t += [dt.now()]
+
+    run_details = wait_for_run_to_complete(run_id)  # noqa F841
+    t += [dt.now()]
+
+    with open(OUTPUT_FILE, "a") as f:
+        time_deltas = [str(t[i + 1] - t[i]) for i in range(len(t) - 1)]
+        f.write(OUTPUT_SEP.join([run_name] + time_deltas) + "\n")
+
+    return run_details
+
+
+def run_concurrently(pipelinefunc_name_tuples: [(Callable, str)]) -> [(str, str)]:
+    pipeline_status = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        performance_tests = (
+            executor.submit(run_single_pipeline_performance_test, func, name)
+            for (func, name) in pipelinefunc_name_tuples
+        )
+        for performance_test in concurrent.futures.as_completed(performance_tests):
+            try:
+                run_details = performance_test.result()
+                pipeline_status.append((run_details.name, run_details.status))
+            except Exception as e:
+                error = f"{e.__class__.__name__}: {str(e)}"
+                print(error)
+                pipeline_status.append(("unknown pipeline", error))
+
+    return pipeline_status
+
+
+def run_performance_tests():
+
+    with open(OUTPUT_FILE, "w") as f:
+        f.write(OUTPUT_SEP.join(["Pipeline", "Compile", "Submit", "Run"]) + "\n")
 
     pipeline_functions = load_pipeline_functions()
 
-    for func, name in pipeline_functions:
-        t = [dt.now()]
-
-        pipeline_file = compile_pipeline(func)
-        t += [dt.now()]
-
-        run_id = run_pipeline(pipeline_file, name)
-        t += [dt.now()]
-
-        run_details = wait_for_run_to_complete(run_id)  # noqa F841
-        t += [dt.now()]
-
-        with open(output_file, "a") as f:
-            time_deltas = [str(t[i + 1] - t[i]) for i in range(len(t) - 1)]
-            f.write(output_sep.join([name] + time_deltas) + "\n")
+    if NUM_WORKERS == 1:  # TODO: use `run_concurrently()` even with 1 worker
+        for func, name in pipeline_functions:
+            run_single_pipeline_performance_test(func, name)
+    else:
+        run_concurrently(pipeline_functions)
 
 
 if __name__ == '__main__':
-    run_performance_test()
+
+    run_performance_tests()
 
     # client = get_client()
-    # experiments = client.list_experiments(namespace='mlx')
-    # experiment = client.create_experiment(name='PERF_TESTS', namespace='mlx')
-    # experiment = client.get_experiment(experiment_name='PERF_TESTS')
-    # runs = client.list_runs(experiment_id=experiment.id, page_size=100)
-    # print(experiments)
+    # from kfp_server_api import ApiListExperimentsResponse, ApiExperiment, ApiListRunsResponse
+    # experiments: ApiListExperimentsResponse = client.list_experiments()
+    # experiment: ApiExperiment = client.create_experiment(name='PERF_TESTS')
+    # experiment: ApiExperiment = client.get_experiment(experiment_name='PERF_TEST')
+    # runs: ApiListRunsResponse = client.list_runs(experiment_id=experiment.id, page_size=100)
+    # print("Experiments: " + ", ".join([e.name for e in experiments.experiments]))
+    # print("Runs: " + ", ".join([r.name for r in runs.runs]))
