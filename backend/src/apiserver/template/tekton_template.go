8e53c3e54dd8f5554d298c0a15aa968c2ce4046c
Yihong Wang <yh.wang@ibm.com>
2022-05-20 09:37:52 -0700

    disable eof strip if results are moved
    when results are moved to /tekton/home/tep-results,
    the eof strip shalln't be applied. disable it in
    this case for now.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- backend/src/apiserver/template/tekton_template.go
+++ backend/src/apiserver/template/tekton_template.go
@@ -294,9 +294,24 @@ func (t *Tekton) injectArchivalStep(workflow util.Workflow, artifactItemsJSON ma
 					task.TaskSpec.StepTemplate.VolumeMounts = append(task.TaskSpec.StepTemplate.VolumeMounts, loggingVolumeMounts...)
 				}
 
+				moveStep := false
+				if task.TaskSpec.Results != nil && len(task.TaskSpec.Results) > 0 {
+					// move all results to /tekton/home/tep-results to avoid result duplication in copy-artifacts step
+					// TODO: disable eof strip, since no results under /tekton/results after this step
+					moveResults := workflowapi.Step{Container: corev1.Container{
+						Image:   "busybox",
+						Name:    "move-all-results-to-tekton-home",
+						Command: []string{"sh", "-c"},
+						Args: []string{fmt.Sprintf("if [ -d /tekton/results ]; then mkdir -p %s; mv /tekton/results/* %s/ || true; fi\n",
+							common.GetPath4InternalResults(), common.GetPath4InternalResults())},
+					}}
+					moveStep = true
+					task.TaskSpec.Steps = append(task.TaskSpec.Steps, moveResults)
+				}
+
 				// Process the artifacts into minimum sh commands if running with minimum linux kernel
 				if injectDefaultScript {
-					artifactScript = t.injectDefaultScript(workflow, artifactScript, artifacts, hasArtifacts, archiveLogs, trackArtifacts, stripEOF)
+					artifactScript = t.injectDefaultScript(workflow, artifactScript, artifacts, hasArtifacts, archiveLogs, trackArtifacts, stripEOF, moveStep)
 				}
 
 				// Define post-processing step
@@ -327,19 +342,6 @@ func (t *Tekton) injectArchivalStep(workflow util.Workflow, artifactItemsJSON ma
 
 				step := workflowapi.Step{Container: container}
 
-				if task.TaskSpec.Results != nil && len(task.TaskSpec.Results) > 0 {
-					// move all results to /tekton/home/tep-results to avoid result duplication in copy-artifacts step
-					// TODO: this causes eof strip fails, since no results under /tekton/results after this step
-					moveResults := workflowapi.Step{Container: corev1.Container{
-						Image:   "busybox",
-						Name:    "move-all-results-to-tekton-home",
-						Command: []string{"sh", "-c"},
-						Args: []string{fmt.Sprintf("if [ -d /tekton/results ]; then mkdir -p %s; mv /tekton/results/* %s/ || true; fi\n",
-							common.GetPath4InternalResults(), common.GetPath4InternalResults())},
-					}}
-					task.TaskSpec.Steps = append(task.TaskSpec.Steps, moveResults)
-				}
-
 				task.TaskSpec.Steps = append(task.TaskSpec.Steps, step)
 			}
 		}
@@ -347,7 +349,7 @@ func (t *Tekton) injectArchivalStep(workflow util.Workflow, artifactItemsJSON ma
 }
 
 func (t *Tekton) injectDefaultScript(workflow util.Workflow, artifactScript string,
-	artifacts [][]interface{}, hasArtifacts bool, archiveLogs bool, trackArtifacts bool, stripEOF bool) string {
+	artifacts [][]interface{}, hasArtifacts, archiveLogs, trackArtifacts, stripEOF, hasMoveStep bool) string {
 	// Need to represent as Raw String Literals
 	artifactScript += "\n"
 	if archiveLogs {
@@ -367,7 +369,7 @@ func (t *Tekton) injectDefaultScript(workflow util.Workflow, artifactScript stri
 	}
 
 	// Strip EOF if enabled, do it after artifact upload since it only applies to parameter outputs
-	if hasArtifacts && len(artifacts) > 0 && stripEOF {
+	if !hasMoveStep && hasArtifacts && len(artifacts) > 0 && stripEOF {
 		for _, artifact := range artifacts {
 			if len(artifact) == 2 {
 				// The below solution is in experimental stage and didn't cover all edge cases.
