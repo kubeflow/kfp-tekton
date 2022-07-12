4ba74bafbe884745b53fd694c8a24cec3484a1f0
Tommy Li <Tommy.chaoping.li@ibm.com>
2022-07-12 15:54:24 -0700

    fix typo

--- sdk/python/kfp_tekton/compiler/_data_passing_rewriter.py
+++ sdk/python/kfp_tekton/compiler/_data_passing_rewriter.py
@@ -634,7 +634,7 @@ def big_data_passing_tasks(prname: str, task: dict, pipelinerun_template: dict,
                         dst = '$(results.%s.path)' % sanitize_k8s_name(result['name'])
                         if artifact_name == result['name'] and src != dst:
                             add_copy_results_artifacts_step = True
-                            total_size_command = 'ARTIFACT_SIZE=`wc -c %s | awk \'{print $1}\'`\n' % src + \ 
+                            total_size_command = 'ARTIFACT_SIZE=`wc -c %s | awk \'{print $1}\'`\n' % src + \
                                                  'TOTAL_SIZE=$( expr $TOTAL_SIZE + $ARTIFACT_SIZE)\n'
                             copy_command = '    cp ' + src + ' ' + dst + '\n'
                             if env.get('OUTPUT_PREVIEW', 'false').lower() == 'true':
