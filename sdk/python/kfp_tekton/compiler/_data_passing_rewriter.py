cfa91b5c60312a87523e5cf8779fe00f6444ac09
Tommy Li <Tommy.chaoping.li@ibm.com>
2022-07-18 14:54:56 -0700

    fix multi input step bug

--- sdk/python/kfp_tekton/compiler/_data_passing_rewriter.py
+++ sdk/python/kfp_tekton/compiler/_data_passing_rewriter.py
@@ -701,16 +701,28 @@ def input_artifacts_tasks(template: dict, artifact: dict) -> dict:
     volume_template = []
     mounted_param_paths = []
     copy_inputs_step = _get_base_step('copy-inputs')
+    has_copy_input = False
+    copy_input_index = 0
     if 'raw' in artifact:
-        copy_inputs_step['command'].append('set -exo pipefail\necho -n "%s" > %s\n' % (
-            artifact['raw']['data'], artifact['path']))
+        for step_index in range(len(template['taskSpec']['steps'])):
+            if template['taskSpec']['steps'][step_index].get('name') == 'copy-inputs':
+                copy_input_index = step_index
+                has_copy_input = True
+        if has_copy_input:
+            template['taskSpec']['steps'][copy_input_index]['command'][-1] = \
+            template['taskSpec']['steps'][copy_input_index]['command'][-1] + \
+            'echo -n "%s" > %s\n' % (artifact['raw']['data'], artifact['path'])
+        else:
+            copy_inputs_step['command'].append('set -exo pipefail\necho -n "%s" > %s\n' % (
+                artifact['raw']['data'], artifact['path']))
     mount_path = artifact['path'].rsplit("/", 1)[0]
     if mount_path not in mounted_param_paths:
         _add_mount_path(artifact['name'], artifact['path'], mount_path,
                         volume_mount_step_template, volume_template,
                         mounted_param_paths)
-    template['taskSpec']['steps'] = _prepend_steps(
-        [copy_inputs_step], template['taskSpec']['steps'])
+    if not has_copy_input:
+        template['taskSpec']['steps'] = _prepend_steps(
+            [copy_inputs_step], template['taskSpec']['steps'])
     # _update_volumes(template, volume_mount_step_template, volume_template)
     if volume_mount_step_template:
         template['taskSpec']['stepTemplate'] = template['taskSpec'].get('stepTemplate', {})
