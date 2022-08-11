9aedd8f0c3b5ea2e7e16074d43b885259289916f
Tommy Li <Tommy.chaoping.li@ibm.com>
2022-08-11 11:23:56 -0700

    update pipelineloop to remove old Tekton alpha resources

--- tekton-catalog/pipeline-loops/test/controller.go
+++ tekton-catalog/pipeline-loops/test/controller.go
@@ -36,7 +36,6 @@ import (
 	faketaskinformer "github.com/tektoncd/pipeline/pkg/client/injection/informers/pipeline/v1beta1/task/fake"
 	faketaskruninformer "github.com/tektoncd/pipeline/pkg/client/injection/informers/pipeline/v1beta1/taskrun/fake"
 	fakeresourceclientset "github.com/tektoncd/pipeline/pkg/client/resource/clientset/versioned/fake"
-	resourceinformersv1alpha1 "github.com/tektoncd/pipeline/pkg/client/resource/informers/externalversions/resource/v1alpha1"
 	fakeresourceclient "github.com/tektoncd/pipeline/pkg/client/resource/injection/client/fake"
 	fakeresourceinformer "github.com/tektoncd/pipeline/pkg/client/resource/injection/informers/resource/v1alpha1/pipelineresource/fake"
 	cloudeventclient "github.com/tektoncd/pipeline/pkg/reconciler/events/cloudevent"
@@ -61,18 +60,16 @@ import (
 // Data represents the desired state of the system (i.e. existing resources) to seed controllers
 // with.
 type Data struct {
-	PipelineRuns      []*v1beta1.PipelineRun
-	Pipelines         []*v1beta1.Pipeline
-	TaskRuns          []*v1beta1.TaskRun
-	Tasks             []*v1beta1.Task
-	ClusterTasks      []*v1beta1.ClusterTask
-	PipelineResources []*v1alpha1.PipelineResource
-	Conditions        []*v1alpha1.Condition
-	Runs              []*v1alpha1.Run
-	Pods              []*corev1.Pod
-	Namespaces        []*corev1.Namespace
-	ConfigMaps        []*corev1.ConfigMap
-	ServiceAccounts   []*corev1.ServiceAccount
+	PipelineRuns    []*v1beta1.PipelineRun
+	Pipelines       []*v1beta1.Pipeline
+	TaskRuns        []*v1beta1.TaskRun
+	Tasks           []*v1beta1.Task
+	ClusterTasks    []*v1beta1.ClusterTask
+	Runs            []*v1alpha1.Run
+	Pods            []*corev1.Pod
+	Namespaces      []*corev1.Namespace
+	ConfigMaps      []*corev1.ConfigMap
+	ServiceAccounts []*corev1.ServiceAccount
 }
 
 // Clients holds references to clients which are useful for reconciler tests.
@@ -85,17 +82,15 @@ type Clients struct {
 
 // Informers holds references to informers which are useful for reconciler tests.
 type Informers struct {
-	PipelineRun      informersv1beta1.PipelineRunInformer
-	Pipeline         informersv1beta1.PipelineInformer
-	TaskRun          informersv1beta1.TaskRunInformer
-	Run              informersv1alpha1.RunInformer
-	Task             informersv1beta1.TaskInformer
-	ClusterTask      informersv1beta1.ClusterTaskInformer
-	PipelineResource resourceinformersv1alpha1.PipelineResourceInformer
-	Condition        informersv1alpha1.ConditionInformer
-	Pod              coreinformers.PodInformer
-	ConfigMap        coreinformers.ConfigMapInformer
-	ServiceAccount   coreinformers.ServiceAccountInformer
+	PipelineRun    informersv1beta1.PipelineRunInformer
+	Pipeline       informersv1beta1.PipelineInformer
+	TaskRun        informersv1beta1.TaskRunInformer
+	Run            informersv1alpha1.RunInformer
+	Task           informersv1beta1.TaskInformer
+	ClusterTask    informersv1beta1.ClusterTaskInformer
+	Pod            coreinformers.PodInformer
+	ConfigMap      coreinformers.ConfigMapInformer
+	ServiceAccount coreinformers.ServiceAccountInformer
 }
 
 // Assets holds references to the controller, logs, clients, and informers.
@@ -212,13 +207,6 @@ func SeedTestData(t *testing.T, ctx context.Context, d Data) (Clients, Informers
 	for _, ct := range d.ClusterTasks {
 		ct := ct.DeepCopy() // Avoid assumptions that the informer's copy is modified.
 		if _, err := c.Pipeline.TektonV1beta1().ClusterTasks().Create(ctx, ct, metav1.CreateOptions{}); err != nil {
-			t.Fatal(err)
-		}
-	}
-	c.Resource.PrependReactor("*", "pipelineresources", AddToInformer(t, i.PipelineResource.Informer().GetIndexer()))
-	for _, r := range d.PipelineResources {
-		r := r.DeepCopy() // Avoid assumptions that the informer's copy is modified.
-		if _, err := c.Resource.TektonV1alpha1().PipelineResources(r.Namespace).Create(ctx, r, metav1.CreateOptions{}); err != nil {
 			t.Fatal(err)
 		}
 	}
