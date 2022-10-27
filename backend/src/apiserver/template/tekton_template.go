72a033bb84f36124fa4283cf35cb5a07f7578117
Tommy Li <Tommy.chaoping.li@ibm.com>
2022-10-27 10:13:15 -0400

    fix path

--- backend/src/apiserver/template/tekton_template.go
+++ backend/src/apiserver/template/tekton_template.go
@@ -371,13 +371,10 @@ func (t *Tekton) injectDefaultScript(workflow util.Workflow, artifactScript stri
 	if hasArtifacts && len(artifacts) > 0 && trackArtifacts {
 		for _, artifact := range artifacts {
 			artifactBaseName := fmt.Sprintf("\"$(basename \"%s\")", artifact[1])
-			if artifact[0] == "mlpipeline-ui-metadata" {
-				artifactBaseName = "mlpipeline_ui_metadata/data\""
-			}
-			if artifact[0] == "mlpipeline-metrics" {
-				artifactBaseName = "mlpipeline_metrics/data\""
-			}
 			artifactPathName := fmt.Sprintf("\"%s/%s", common.GetPath4InternalResults(), artifactBaseName)
+			if artifact[0] == "mlpipeline-ui-metadata" || artifact[0] == "mlpipeline-metrics" {
+				artifactPathName = fmt.Sprintf("\"%s\"", artifact[1])
+			}
 			if len(artifact) == 2 {
 				artifactScript += fmt.Sprintf("push_artifact \"%s\" %s\n",
 					artifact[0], artifactPathName)
