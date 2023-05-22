2bbaebb61ffd78402ccdf5e5795b0e875b59a4be
Tommy Li <Tommy.chaoping.li@ibm.com>
2023-05-22 14:28:35 -0700

    optimize persisten agent to pass yaml directly to db and minio client

--- backend/src/agent/persistence/client/pipeline_client.go
+++ backend/src/agent/persistence/client/pipeline_client.go
@@ -20,10 +20,10 @@ import (
 	"time"
 
 	api "github.com/kubeflow/pipelines/backend/api/v1/go_client"
+	kfpResource "github.com/kubeflow/pipelines/backend/src/apiserver/resource"
+	kfpServer "github.com/kubeflow/pipelines/backend/src/apiserver/server"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
 	"github.com/pkg/errors"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 const (
@@ -42,6 +42,7 @@ type PipelineClient struct {
 	timeout             time.Duration
 	reportServiceClient api.ReportServiceClient
 	runServiceClient    api.RunServiceClient
+	resourceManager     *kfpResource.ResourceManager
 }
 
 func NewPipelineClient(
@@ -53,6 +54,8 @@ func NewPipelineClient(
 	mlPipelineServiceGRPCPort string) (*PipelineClient, error) {
 	httpAddress := fmt.Sprintf(addressTemp, mlPipelineServiceName, mlPipelineServiceHttpPort)
 	grpcAddress := fmt.Sprintf(addressTemp, mlPipelineServiceName, mlPipelineServiceGRPCPort)
+	clientManager := newClientManager()
+	resourceManager := kfpResource.NewResourceManager(&clientManager)
 	err := util.WaitForAPIAvailable(initializeTimeout, basePath, httpAddress)
 	if err != nil {
 		return nil, errors.Wrapf(err,
@@ -69,6 +72,7 @@ func NewPipelineClient(
 		timeout:             timeout,
 		reportServiceClient: api.NewReportServiceClient(connection),
 		runServiceClient:    api.NewRunServiceClient(connection),
+		resourceManager:     resourceManager,
 	}, nil
 }
 
@@ -76,77 +80,41 @@ func (p *PipelineClient) ReportWorkflow(workflow *util.Workflow) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	_, err := p.reportServiceClient.ReportWorkflow(ctx, &api.ReportWorkflowRequest{
-		Workflow: workflow.ToStringForStore(),
-	})
+	err := p.resourceManager.ReportWorkflowResource(ctx, workflow)
 
 	if err != nil {
-		statusCode, _ := status.FromError(err)
-		if statusCode.Code() == codes.InvalidArgument || statusCode.Code() == codes.NotFound {
-			// Do not retry if either:
-			// * there is something wrong with the workflow
-			// * the workflow has been deleted by someone else
-			return util.NewCustomError(err, util.CUSTOM_CODE_PERMANENT,
-				"Error while reporting workflow resource (code: %v, message: %v): %v, %+v",
-				statusCode.Code(),
-				statusCode.Message(),
-				err.Error(),
-				workflow.PipelineRun)
-		} else {
-			// Retry otherwise
-			return util.NewCustomError(err, util.CUSTOM_CODE_TRANSIENT,
-				"Error while reporting workflow resource (code: %v, message: %v): %v, %+v",
-				statusCode.Code(),
-				statusCode.Message(),
-				err.Error(),
-				workflow.PipelineRun)
-		}
+		return util.Wrap(err, "Report workflow failed.")
 	}
+
 	return nil
 }
 
 func (p *PipelineClient) ReportScheduledWorkflow(swf *util.ScheduledWorkflow) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	_, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	_, err := p.reportServiceClient.ReportScheduledWorkflow(ctx,
-		&api.ReportScheduledWorkflowRequest{
-			ScheduledWorkflow: swf.ToStringForStore(),
-		})
+	err := p.resourceManager.ReportScheduledWorkflowResource(swf)
 
 	if err != nil {
-		statusCode, _ := status.FromError(err)
-		if statusCode.Code() == codes.InvalidArgument {
-			// Do not retry if there is something wrong with the workflow
-			return util.NewCustomError(err, util.CUSTOM_CODE_PERMANENT,
-				"Error while reporting workflow resource (code: %v, message: %v): %v, %+v",
-				statusCode.Code(),
-				statusCode.Message(),
-				err.Error(),
-				swf.ScheduledWorkflow)
-		} else {
-			// Retry otherwise
-			return util.NewCustomError(err, util.CUSTOM_CODE_TRANSIENT,
-				"Error while reporting workflow resource (code: %v, message: %v): %v, %+v",
-				statusCode.Code(),
-				statusCode.Message(),
-				err.Error(),
-				swf.ScheduledWorkflow)
-		}
+		return util.Wrap(err, "Report scheduled workflow failed.")
 	}
+
 	return nil
 }
 
 // ReadArtifact reads artifact content from run service. If the artifact is not present, returns
 // nil response.
 func (p *PipelineClient) ReadArtifact(request *api.ReadArtifactRequest) (*api.ReadArtifactResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	_, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	response, err := p.runServiceClient.ReadArtifact(ctx, request)
+	content, err := p.resourceManager.ReadArtifact(request.GetRunId(), request.GetNodeId(), request.GetArtifactName())
 	if err != nil {
-		// TODO(hongyes): check NotFound error code before skip the error.
+		// TODO: check NotFound error code before skip the error.
 		return nil, nil
+	}
+	response := &api.ReadArtifactResponse{
+		Data: content,
 	}
 
 	return response, nil
@@ -154,15 +122,25 @@ func (p *PipelineClient) ReadArtifact(request *api.ReadArtifactRequest) (*api.Re
 
 // ReportRunMetrics reports run metrics to run service.
 func (p *PipelineClient) ReportRunMetrics(request *api.ReportRunMetricsRequest) (*api.ReportRunMetricsResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	_, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	response, err := p.runServiceClient.ReportRunMetrics(ctx, request)
+	// Makes sure run exists
+	_, err := p.resourceManager.GetRun(request.GetRunId())
 	if err != nil {
-		// This call should always succeed unless the run doesn't exist or server is broken. In
-		// either cases, the job should retry at a later time.
-		return nil, util.NewCustomError(err, util.CUSTOM_CODE_TRANSIENT,
-			"Error while reporting metrics (%+v): %+v", request, err)
+		return nil, err
+	}
+	response := &api.ReportRunMetricsResponse{
+		Results: []*api.ReportRunMetricsResponse_ReportRunMetricResult{},
+	}
+	for _, metric := range request.GetMetrics() {
+		err := kfpServer.ValidateRunMetric(metric)
+		if err == nil {
+			err = p.resourceManager.ReportMetric(metric, request.GetRunId())
+		}
+		response.Results = append(
+			response.Results,
+			kfpServer.NewReportRunMetricResult(metric.GetName(), metric.GetNodeId(), err))
 	}
 	return response, nil
 }
