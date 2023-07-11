5962697db4d95e28c545ff6c9696964a7cc55dce
Yihong Wang <yh.wang@ibm.com>
2023-07-10 22:27:03 -0700

    test(v2): add end-2-end test (#1275)
    Add a GitHub action for end-2-end test:
    - compile flip coin Python script
    - submit IR to api server
    - create a run
    - verify the run result
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

---
+++ scripts/deploy/github/build-images.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright 2023 kubeflow.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/check_registry.sh
+
+# Remove the x if you need no print out of each command
+set -e
+
+REGISTRY="${REGISTRY:-kind-registry:5000}"
+
+docker build -t "${REGISTRY}/apiserver:latest" -f backend/Dockerfile . && docker push "${REGISTRY}/apiserver:latest"
+docker build -t "${REGISTRY}/persistenceagent:latest" -f backend/Dockerfile.persistenceagent . && docker push "${REGISTRY}/persistenceagent:latest"
+docker build -t "${REGISTRY}/scheduledworkflow:latest" -f backend/Dockerfile.scheduledworkflow . && docker push "${REGISTRY}/scheduledworkflow:latest"
+docker build -t "${REGISTRY}/tekton-driver:latest" -f backend/Dockerfile.tektondriver . && docker push "${REGISTRY}/tekton-driver:latest" &
+docker build -t "${REGISTRY}/tekton-exithandler-controller:latest" -f backend/Dockerfile.tekton-exithandler.controller . && docker push "${REGISTRY}/tekton-exithandler-controller:latest" &
+docker build -t "${REGISTRY}/tekton-exithandler-webhook:latest" -f backend/Dockerfile.tekton-exithandler.webhook . && docker push "${REGISTRY}/tekton-exithandler-webhook:latest"
+docker build -t "${REGISTRY}/tekton-kfptask-controller:latest" -f backend/Dockerfile.tekton-kfptask.controller . && docker push "${REGISTRY}/tekton-kfptask-controller:latest" &
+docker build -t "${REGISTRY}/tekton-kfptask-webhook:latest" -f backend/Dockerfile.tekton-kfptask.webhook . && docker push "${REGISTRY}/tekton-kfptask-webhook:latest" &
+
+wait
+
+# clean up intermittent build caches to free up disk space
+docker system prune -a -f
