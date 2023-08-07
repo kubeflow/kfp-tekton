fa6f4b70d402121c42b98920f6d222062473baeb
Tommy Li <Tommy.chaoping.li@ibm.com>
2023-08-07 09:12:45 -0700

    update deps to fix python 3.10 error

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -54,7 +54,7 @@ development stage. Contributions are welcome: {}
 # NOTICE, after any updates to the following, ./requirements.in should be updated
 # accordingly.
 REQUIRES = [
-    "kfp>=1.8.10,<1.8.22",
+    "kfp>=1.8.10,<1.8.23",
 ]
 
 TESTS_REQUIRE = [
